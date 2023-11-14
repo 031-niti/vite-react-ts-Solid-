1343a9ae4df001772a29f22b1a3ceaa4a5a96fe8
Niti <earthza6263@gmail.com>
2023-11-14 13:56:39 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import BMICalculatorComponent from './components/BMICalculatorComponent'
+
+
+function App() {
+  return (
+    <>
+     <div className="App">
+      <BMICalculatorComponent />
+    </div>
+    </>
+  )
+}
+
+export default App
